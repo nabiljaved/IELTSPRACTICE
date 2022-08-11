fee1d6f20651c4fc5b8a9fbfd7f34f11d78c6725
vishnuprasad-animator <nabeeljaved2016@gmail.com>
2022-08-11 13:17:54 +0500

    side bar completed

---
+++ app/views/academic-reading/js/app.js
@@ -0,0 +1,10 @@
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+var openNav = document.querySelector(".open");
+
+toggleButton.addEventListener("click", function() {
+
+    mobileNav.classList.toggle("open");
+    
+  });
+  
